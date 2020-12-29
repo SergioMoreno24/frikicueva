193ce27cc5b5bf122c0055ce9955989565113678
Sergio Moreno <sergio@ensenapormexico.org>
2020-12-29 12:56:36 -0600

    Primera entrega de proyecto final

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch, Route } from "react-router-dom";
+import ItemListContainer from './ItemListContainer.js';
+import ItemDetailContainer from './ItemDetailContainer.js';
+//import Home from './Home.js';
+
+const Main = () => {
+    return (
+        <main>
+            <Switch>
+
+                <Route exact path="/">
+                    <ItemListContainer/>
+                </Route>
+
+                <Route exact path="/category">
+                    <ItemListContainer/>
+                </Route>
+
+                <Route exact path="/item/:id">
+                    <ItemDetailContainer />
+                </Route>
+
+            </Switch>
+      </main>
+    )
+}
+
+export default Main;
